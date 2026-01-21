d784bcdb5a66697e5460badcecb3599396ccee42
davidtaylor13 <davidtay0907@gmail.com>
2026-01-20 19:24:56 -0700

    Creates a pyramid of stars now

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
 /*
 Make a program where you enter the height of a pyramid
 and the program will print out that pyramid.
 
 For example, if I was to type 5, it would show
-     *
-    * *
-   * * *
-  * * * *
-* * * * * *
+    
+    *
+   ***
+  *****
+ *******
+*********
 
 If I was to type 3 it would show
    *
@@ -15,3 +21,23 @@ If I was to type 3 it would show
 * * * *
 
 */
+int main()
+{
+    int num = 0;
+    cout << "Type an integer: ";
+    cin >> num;
+    for (int i = 1; i <= num; i++)
+    {
+        for (int j = 1; j <= num - i; j++)
+        {
+            cout << " ";
+        }
+
+        for (int star = 1; star <= (i * 2) - 1; star++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
